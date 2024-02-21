fad02447d98b90e7872465e01d9c94b141ef1c23
Michael Maggiore <michaelpmaggiore@gmail.com>
2024-02-21 10:35:13 -0700

    Initial Commit

---
+++ ARGoogleMaps/ARGoogleMaps-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ARGoogleMaps-Bridging-Header.h
+//  ARGoogleMaps
+//
+//  Created by Michael Maggiore on 2/21/24.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
