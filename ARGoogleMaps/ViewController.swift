d429912a5dc21b378ec0b65bb2e04b3d95d4c02a
Michael Maggiore <michaelpmaggiore@gmail.com>
2024-02-23 17:08:23 -0700

    Added sidebar

--- ARGoogleMaps/ViewController.swift
+++ ARGoogleMaps/ViewController.swift
@@ -16,10 +16,17 @@ import CoreLocation
 extension MTKView : RenderDestinationProvider {
 }
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, HamburgerViewControllerDelegate {
     @IBOutlet fileprivate weak var mapView: GMSMapView!
     var session: ARSession!
     var renderer: Renderer!
+    @IBOutlet weak var mainBackView: UIView!
+    @IBOutlet weak var hamburgerView: UIView!
+    @IBOutlet weak var mappingView: UIView!
+
+    @IBOutlet fileprivate weak var leadingConstraintForHamburgerView: NSLayoutConstraint!
+    
+    @IBOutlet weak var backViewForHamburger: UIView!
     
     let manager = CLLocationManager()
     
@@ -28,6 +35,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         manager.delegate = self
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+        self.backViewForHamburger.isHidden = true
+//        self.mainBackView.layer.cornerRadius = 40
+        self.mainBackView.layer.cornerRadius = 55
+        self.mainBackView.clipsToBounds = true
   }
   
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -37,8 +48,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         let coordinate = location.coordinate
         let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        self.view.addSubview(mapView)
+        let mapView = GMSMapView.map(withFrame: self.mappingView.frame, camera: camera)
+        self.mappingView.addSubview(mapView)
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
@@ -47,4 +58,121 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         marker.snippet = "Australia"
         marker.map = mapView
     }
+    
+    // BELOW IS NOT MY CODE:
+    @IBAction func tappedOnHamburgerbackView(_ sender: Any) {
+           self.hideHamburgerView()
+       }
+       
+       func hideHamburgerMenu() {
+           self.hideHamburgerView()
+       }
+       
+       private func hideHamburgerView()
+       {
+           UIView.animate(withDuration: 0.1) {
+               self.leadingConstraintForHamburgerView.constant = 10
+               self.view.layoutIfNeeded()
+           } completion: { (status) in
+               self.backViewForHamburger.alpha = 0.0
+               UIView.animate(withDuration: 0.1) {
+                   self.leadingConstraintForHamburgerView.constant = -280
+                   self.view.layoutIfNeeded()
+               } completion: { (status) in
+                   self.backViewForHamburger.isHidden = true
+                   self.isHamburgerMenuShown = false
+               }
+           }
+       }
+    @IBAction func showHamburgerMenu(_ sender: Any) {
+            UIView.animate(withDuration: 0.1) {
+                self.leadingConstraintForHamburgerView.constant = 10
+                self.view.layoutIfNeeded()
+            } completion: { (status) in
+                self.backViewForHamburger.alpha = 0.75
+                self.backViewForHamburger.isHidden = false
+                UIView.animate(withDuration: 0.1) {
+                    self.leadingConstraintForHamburgerView.constant = 0
+                    self.view.layoutIfNeeded()
+                } completion: { (status) in
+                    self.isHamburgerMenuShown = true
+                }
+
+            }
+
+            self.backViewForHamburger.isHidden = false
+            
+        }
+        
+        var hamburgerViewController : HamburgerViewController?
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if (segue.identifier == "hamburgerSegue")
+            {
+                if let controller = segue.destination as? HamburgerViewController
+                {
+                    self.hamburgerViewController = controller
+                    self.hamburgerViewController?.delegate = self
+                }
+            }
+        }
+        
+        private var isHamburgerMenuShown:Bool = false
+        private var beginPoint:CGFloat = 0.0
+        private var difference:CGFloat = 0.0
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            if (isHamburgerMenuShown)
+            {
+                 if let touch = touches.first
+                {
+                    let location = touch.location(in: backViewForHamburger)
+                    beginPoint = location.x
+                }
+            }
+        }
+        
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            if (isHamburgerMenuShown)
+            {
+                if let touch = touches.first
+                {
+                    let location = touch.location(in: backViewForHamburger)
+                    
+                    let differenceFromBeginPoint = beginPoint - location.x
+                    
+                    if (differenceFromBeginPoint>0 || differenceFromBeginPoint<280)
+                    {
+                        difference = differenceFromBeginPoint
+                        self.leadingConstraintForHamburgerView.constant = -differenceFromBeginPoint
+                        self.backViewForHamburger.alpha = 0.75-(0.75*differenceFromBeginPoint/280)
+                    }
+                }
+            }
+        }
+        
+        override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            if (isHamburgerMenuShown)
+            {
+                if (difference>140)
+                {
+                    UIView.animate(withDuration: 0.1) {
+                        self.leadingConstraintForHamburgerView.constant = -290
+                    } completion: { (status) in
+                        self.backViewForHamburger.alpha = 0.0
+                        self.isHamburgerMenuShown = false
+                        self.backViewForHamburger.isHidden = true
+                    }
+                }
+                else{
+                    UIView.animate(withDuration: 0.1) {
+                        self.leadingConstraintForHamburgerView.constant = -10
+                    } completion: { (status) in
+                        self.backViewForHamburger.alpha = 0.75
+                        self.isHamburgerMenuShown = true
+                        self.backViewForHamburger.isHidden = false
+                    }
+                }
+            }
+        }
 }
